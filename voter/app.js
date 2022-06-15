3fb9c23e132d15d1e05a009dbd06b127cbe6754c
mschrage <michael.schrage@fincon.eu>
2022-06-15 15:00:34 +0200

    Bis einschließlich v-for

--- voter/app.js
+++ voter/app.js
@@ -3,7 +3,56 @@ const app = Vue.createApp({
   data: function () {
     return {
       mysubmissions: submissions, // aus seed.js
+      totalVotes: 0
     };
+  },
+  computed: {
+    /*totalVotes() {
+      console.log("computed property ausgeführt.");
+      return this.mysubmissions.reduce((totalVotes, submission) => {
+        return totalVotes + submission.votes;
+      }, 0);
+    }*/
+    sortedSubmissions() {
+      return this.mysubmissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
+  },
+  methods: {
+    /*upvote: function () {},*/
+    upvote() {
+      console.log();
+      this.mysubmissions[0].votes++;
+    },
+    logConsole(text) {
+      console.log(text);
+    },
+    /*totalVotes() {
+      return this.mysubmissions.reduce((totalVotes, submission) => {
+        return totalVotes + submission.votes;
+      }, 0);
+    }*/
+  },
+
+  watch: {
+    /*mysubmissions(newValue, oldValue) {
+      console.log(newValue);
+      console.log(oldValue);
+    },*/
+    mysubmissions: {
+      handler() {
+        this.totalVotes = this.mysubmissions.reduce((totalVotes, submission) => {
+          return totalVotes + submission.votes;
+        }, 0);
+      },
+      deep: true,
+      immediate: true
+    },
+    totalVotes(newValue, oldValue) {
+      console.log(newValue);
+      console.log(oldValue);
+    },
   }
 });
 
